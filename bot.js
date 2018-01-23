a41e0e91df753a873a9d9dced1a9f0f89a0d6c52
Developer_Kafir <35732485+DeveloperDiscordSupport@users.noreply.github.com>
2018-01-23 17:38:27 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -12,7 +12,7 @@ client.on('message', message => {
 });
 
 client.on('message', message => {
-    if (message.content === 'bing') {
+    if (message.content === 'היי') {
     	message.reply('$[member]:wink:?היי מה שלומך!!');
   	}
 });
